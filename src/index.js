5ba05c444f2b6755824545146f38d30252bad39d
PODGORNY <oneminutesplease@gmail.com>
2023-09-28 15:44:57 +0300

    correctly structure

--- src/index.js
+++ src/index.js
@@ -1,27 +1,23 @@
-// типа сервис - здесь подключены все части Реакт и Редуктора
-// для работы приложения
+// фундамент приложения
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// Router - обёртка для компонентов, чтобы поделить их на части,
-// для того чтобы переключаться как по страницам
-import { BrowserRouter as Router } from 'react-router-dom';
 // Provider - даёт легкий доступ к глобальным данным, дальним компонентам
 import { Provider } from 'react-redux';
 
+import App from './component/App/App';
 // подключу стор - где редуктор и стэйт
 import store from './Reducer/store';
-// авторизация сервиса
-import { initAuth } from './Reducer/authSlice';
-import App from './component/App/App';
-import './index.css';
 
-store.dispatch(initAuth()).then(() => {
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-  );
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+/*
+// Router - обёртка для компонентов, чтобы поделить их на части,
+// для того чтобы переключаться как по страницам
+import { BrowserRouter as Router } from 'react-router-dom';
+*/
